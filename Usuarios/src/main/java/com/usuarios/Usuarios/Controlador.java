ad54c3d14749fbe2484e99aba807a2f95020c18a
carlos <budaspow@gmail.com>
2020-02-29 16:10:16 +0100

    netBeans Problems. Testing 2

--- Usuarios/src/main/java/com/usuarios/Usuarios/Controlador.java
+++ Usuarios/src/main/java/com/usuarios/Usuarios/Controlador.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//cambios
+//cambios.
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping({"/usuarios"})
