ac0f41278788ee7ffe573c12abedce821fa2ff64
carlos <budaspow@gmail.com>
2020-03-07 18:16:56 +0100

    Pedidos working

---
+++ src/test/java/com/pedidos/Pedidos/PedidoServiceImp.java
@@ -0,0 +1,52 @@
+
+
+import com.pedidos.Pedidos.Pedido;
+import com.pedidos.Pedidos.PedidoRepositorio;
+import com.pedidos.Pedidos.PedidoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class PedidoServiceImp implements PedidoService {
+    @Autowired
+    private PedidoRepositorio repositorio;
+    @Override
+    public List<Pedido> listar() {
+        return repositorio.findAll();
+    }
+
+    @Override
+    public Pedido listarId(int id) {
+        return repositorio.findById(id);
+    }
+    
+    
+
+    @Override
+    public Pedido add(Pedido p) {
+        return repositorio.save(p);
+    }
+
+    @Override
+    public Pedido edit(Pedido p) {
+        return repositorio.save(p);
+    }
+
+
+    @Override
+    public Pedido delete(int id) {
+        Pedido p=repositorio.findById(id);
+        if(p!=null){
+            repositorio.delete(p);
+        }
+        return p;
+
+    }
+
+     
+    
+}
+
